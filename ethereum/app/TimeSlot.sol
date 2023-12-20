b259d5c8a7f24815bf6d9a7590a35a060e3086f8
lmendiboure <leo.mendiboure11@gmail.com>
2023-12-20 15:46:23 +0100

    Update TimeSlot.sol

--- ethereum/app/TimeSlot.sol
+++ ethereum/app/TimeSlot.sol
@@ -14,14 +14,12 @@ contract TimeSlot {
         energyDemands[prosumer] = quantity;
         emit DemandUpdated(prosumer, quantity);
 
-        // Ajouter l'adresse du prosumer à la liste des demandes si ce n'est pas déjà fait
         if (!isInDemandList(prosumer)) {
             demandList.push(prosumer);
         }
     }
 
     function handleRequests() public {
-        // Logique pour traiter les demandes en fonction des priorités
         while (energyAvailable > 0 && demandList.length > 0) {
             address prosumer = getHighestPriorityProsumer();
             if (prosumer != address(0)) {
@@ -31,7 +29,6 @@ contract TimeSlot {
                     energyDemands[prosumer]--;
                     emit RequestHandled(prosumer, quantity);
                 } else {
-                    // Si la demande est épuisée, la retirer de la liste
                     removeFromDemandList(prosumer);
                 }
             } else {
@@ -44,12 +41,10 @@ contract TimeSlot {
         address highestPriorityProsumer = address(0);
         uint highestPriority = 0;
 
-        // Parcourir la liste des demandes
         for (uint i = 0; i < demandList.length; i++) {
             address prosumer = demandList[i];
             uint priority = prosumers[prosumer].priority;
 
-            // Mettre à jour le prosumer avec la priorité la plus élevée
             if (priority > highestPriority && energyDemands[prosumer] > 0) {
                 highestPriority = priority;
                 highestPriorityProsumer = prosumer;
